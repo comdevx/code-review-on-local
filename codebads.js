b85be18b967209633a9344cb3775be92a0a05096
Ororin <comdevx@gmail.com>
2019-02-04 19:50:56 +0700

    updated code and add noSemi noDoubleQuote noDoubleEqual

--- codebads.js
+++ codebads.js
@@ -3,32 +3,56 @@ const lang = require('./lang.json')
 module.exports = (line, countryCode) => {
     const bads = []
     for (let i = 0; i < line.length; i++) {
-        getVar(line[i]) && bads.push({ line: i + 1, msg: lang[countryCode][0].getVar })
-        getNewObject(line[i]) && bads.push({ line: i + 1, msg: lang[countryCode][0].getNewObject })
-        getObjDup(line[i]) && bads.push({ line: i + 1, msg: lang[countryCode][0].getObjDup })
-        getObjQuote(line[i]) && bads.push({ line: i + 1, msg: lang[countryCode][0].getObjQuote })
+        const codeLine = i + 1
+        const msg = lang[countryCode][0]
+        noVar(line[i]) && bads.push({ line: codeLine, msg: msg.noVar })
+        noNewObject(line[i]) && bads.push({ line: codeLine, msg: msg.noNewObject })
+        noObjDup(line[i]) && bads.push({ line: codeLine, msg: msg.noObjDup })
+        noObjQuote(line[i]) && bads.push({ line: codeLine, msg: msg.noObjQuote })
+        noSemi(line[i]) && bads.push({ line: codeLine, msg: msg.noSemi })
+        noDoubleQuote(line[i]) && bads.push({ line: codeLine, msg: msg.noDoubleQuote })
+        noDoubleEqual(line[i]) && bads.push({ line: codeLine, msg: msg.noDoubleEqual })
     }
     return bads
 }
 
-function getVar(val) {
-    const rex = new RegExp('var', "g")
-    return val.search(rex) === 0
+function noVar(val) {
+    return /var/.exec(val)
 }
 
-function getNewObject(val) {
-    const rex = new RegExp('new Object', "g")
-    return val.search(rex) === 0
+function noNewObject(val) {
+    return /new Object/.exec(val)
 }
 
-function getObjDup(val) {
+function noObjDup(val) {
     const rmSpace = val.replace(/\s/g, '')
     const rex = rmSpace.split(':')
     return rex[0] === rex[1]
 }
 
-function getObjQuote(val) {
+function noObjQuote(val) {
+    const semi = /:/.exec(val)
     const rmSpace = val.replace(/\s/g, '')
-    const rex = rmSpace.split(':')
-    return rex[0][0] === '"' || rex[0][0] === "'"
+    if (semi) {
+        const rex = rmSpace.split(':')
+        return /"(.*?)"/.exec(rex[0])
+    }
+}
+
+function noSemi(val) {
+    const noFor = /for/.exec(val)
+    const semi = /;/.exec(val)
+    if (!noFor && semi) {
+        return true
+    }
+}
+
+function noDoubleQuote(val) {
+    const double = /"(.*?)"/.exec(val)
+    const single = /'(.*?)'/.exec(val)
+    return !single && double
+}
+
+function noDoubleEqual(val) {
+    return /\s==\s/.exec(val)
 }
